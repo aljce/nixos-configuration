23b3ebebe4ca3050fbfad51a41a89a7ed934d341
Alice McKean <mckean.kylej@gmail.com>
2020-06-15 23:26:18 -0700

    bios/uefi generic partition script

--- install/iso.nix
+++ install/iso.nix
@@ -1,15 +1,14 @@
 { nixpkgs ? <nixpkgs>, system ? "x86_64-linux" }:
 let configuration =
     { pkgs, ... }:
-    let partition = pkgs.writeScriptBin "partition" "${./partition}";
-    in
     { imports = [
         "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
         ../system/programs.nix
       ];
-      environment.systemPackages = [
-        partition
+      environment.systemPackages = with pkgs; [
+        (writeScriptBin "partition" (builtins.readFile ./partition))
+        git
       ];
     };
     iso-image = import "${nixpkgs}/nixos" { inherit system configuration; };
