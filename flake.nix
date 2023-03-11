f7a6de4a0e30a8863c3639be296df9ec11950441
Alice McKean <mckean.kylej@gmail.com>
2023-03-10 16:28:47 -0800

    upgrade nixos version

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Alice McKean's NixOS configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
