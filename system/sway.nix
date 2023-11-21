20aaab9662ddd802bd38bb4b4378a4a1a8aafe4e
Alice McKean <mckean.kylej@gmail.com>
2023-11-20 19:18:39 -0800

    automatic sway login

--- system/sway.nix
+++ system/sway.nix
@@ -1,3 +1,9 @@
 { pkgs, ... }:
-{ programs.sway.enable = true;
+{ imports = [ ./screen-sharing.nix ];
+  programs.sway.enable = true;
+  environment.loginShellInit = ''
+    if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+          sway --unsupported-gpu -V > .sway-log 2>&1
+    fi
+  '';
 }
